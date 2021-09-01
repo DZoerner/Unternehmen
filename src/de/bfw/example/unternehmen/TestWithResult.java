0aa2d90c15d7ef3ba11c65f706175729837e655f
Dietmar Zoerner <Dietmar.Zoerner@bfw-berlin-brandenburg.de>
2021-09-01 13:57:21 +0200

    Refactored try/catch to old style

--- src/de/bfw/example/unternehmen/TestWithResult.java
+++ src/de/bfw/example/unternehmen/TestWithResult.java
@@ -5,11 +5,12 @@ import java.sql.*;
 public class TestWithResult {
 
     public static void main(String[] args) {
-        try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/unternehmen", "root", "");
+        try{
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/unternehmen", "root", "");
             Statement stmt = conn.createStatement();
 
             ResultSet rs = stmt.executeQuery("SELECT name FROM Kunde");
-        ) {
+
             while(rs.next()){
                 //Display values
                 System.out.println(rs.getString(1));
